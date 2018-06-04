f42df8ce419caca83781eb496cb45f155f547755
Nikolas Schnellbächer <khx0@posteo.net>
2018-06-04 15:49:13 +0200

    first commit

---
+++ compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# compile local TeX files
+# date: 2018-06-04
+##########################################################################################
+
+# tex sheet
+pdflatex main.tex
+bibtex main.aux
+pdflatex main.tex
+pdflatex main.tex
+pdflatex main.tex
+
+# clean up auxiliary files
+rm *.aux *.log  *.out *.toc *.bbl *.blg *.xwm
+
